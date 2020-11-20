19cd705c1aa3956769e3b4201121d029cf5826fc
Iris Kuhn <ichan266@hotmail.com>
2020-11-20 00:54:22 -0800

    Rerun - 2/3 done in arrays.js

--- arrays.js
+++ arrays.js
@@ -11,7 +11,13 @@ function printIndices(items) {
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  let result = [];
+  for (const item in items) {
+    if (item % 2 === 0) {
+      result.push(items[item]);
+    }
+  }
+  console.log(result);
 }
 
 

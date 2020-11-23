64f7b11b05aa8fa3a8ea8f7e1d0aab8e2a1429ac
Iris Kuhn <ichan266@hotmail.com>
2020-11-22 18:57:47 -0800

    objects.js completed

--- objects.js
+++ objects.js
@@ -18,5 +18,15 @@ function countWords(phrase) {
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
-}
+  const melonPrices = {
+    2.50: ['Cantaloup', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+
+  if (! melonPrices[price]) {
+    return;
+  };
+  return melonPrices[price].sort();
+};

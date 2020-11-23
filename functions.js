4d8458cfcaa063319efcc9c5edda548a162ec4ce
Iris Kuhn <ichan266@hotmail.com>
2020-11-22 17:53:45 -0800

    Completed functions.js

--- functions.js
+++ functions.js
@@ -2,15 +2,45 @@
 
 
 // 1. isHometown
+function isHometown(town) {
+  return town === "San Franscisco"
+}
 
-// Define your function here
+const isHometown2 = (town) => {
+  return town === "San Franscisco"
+};
+
+const isHometown3 = (town) => town === "San Franscisco";
+// Note: this one line argument does not need curly braces {}
 
 
 // 2. getFullName
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`
+};
 
-// Define your function here
+const getFullName2 = (firstName, lastName) => {
+  return `${firstName} ${lastName}`
+};
+
+const getFullName3 = (firstName, lastName) => `${firstName} ${lastName}`;
 
 
 // 3. calculateTotal
-
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+  let subtotal = basePrice * (1 + tax);
+  
+  let fee = 0;
+  if (state === 'CA') {
+    fee = 0.03 * subtotal;
+  } else if  (state === 'PA') {
+    fee = 2;
+  } else if (state === 'MA') {
+    if (basePrice <= 100) {
+      fee = 1;
+    } else {
+      fee = 3;
+    };
+  };
+  return (subtotal + fee);
+};
